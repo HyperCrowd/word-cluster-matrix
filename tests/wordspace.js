56937f0d350e9606b2eb01a668d3d95f40db7022
HyperCrowd <76200774+HyperCrowd@users.noreply.github.com>
2023-05-29 12:34:11 -0400

    1 binaries uploaded

--- tests/wordspace.js
+++ tests/wordspace.js
@@ -1,18 +1,16 @@
 const WordTimeClusterReport = require('../wordTimeCluster');
-
-const report = new WordTimeClusterReport(
-  [
-    'is this real',
-    'it is indeed real',
-    'what about now? what is real',
-    'this is not',
-    'my bad homie',
-    'no sweat, stay real',
-  ],
-  [0, 1, 2, 3, 4, 5]
-);
+const { sentence } = require('txtgen/dist/cjs/txtgen.js');
 
 async function main() {
+  const sentences = [];
+  const times = [];
+
+  for (let i = 0; i < 100; i++) {
+    sentences.push(sentence());
+    times.push(i);
+  }
+  const report = new WordTimeClusterReport(sentences, times);
+
   await report.toPng();
 }
 
