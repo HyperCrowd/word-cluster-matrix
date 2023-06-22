f7fd0ebc2a52461e27effce7d943a914ce8dfec0
HyperCrowd <info@hypercrowd.org>
2023-06-22 02:37:49 -0400

    We got working tensorflow

--- cnn.js
+++ cnn.js
@@ -11,23 +11,24 @@ class CNN {
    * 
    */
   constructor ({ 
-    filters = 32, // Number of feature detectors
+    filters = 16, // Number of feature detectors
     kernelSize = 3, // Field size of pattern detection
     height = imageHeight,
     width = imageWidth,
     channels = 4, // 1 = grayscale, 3 = RGB, 4 = RGBa
     poolSize = 2, // Field size of feature aggregation (max or average)
+    strides = 2, // ?
     fullyConnectedLayers = 64,  // Number of fully connected layers
     categories = 0, // Number of categories you want to identify (this is the highest number allowed in the train() method's labels array)
     learningRate = 0.001, // Speed of model adaptivity
     batchSize = 32, // Number of samples used used for each teach training iteration
-    epochs = 3 // Number of times to go through the entire training set
+    epochs = 100 // Number of times to go through the entire training set
   }) {
-    this.model = tf.sequential();
     this.epochs = epochs
     this.batchSize = batchSize
     this.height = height
     this.width = width
+    this.model = tf.sequential();
 
     // Define the model architecture
     this.model.add(tf.layers.conv2d({
@@ -37,19 +38,42 @@ class CNN {
       inputShape: [height, width, channels],
     }));
 
-    this.model.add(tf.layers.maxPooling2d({ poolSize }));
+    this.model.add(tf.layers.maxPooling2d({ poolSize, strides }));
+
+    this.model.add(tf.layers.conv2d({
+      filters: filters * 4,
+      kernelSize,
+      activation: 'relu',
+      inputShape: [height, width, channels],
+    }));
+
+    this.model.add(tf.layers.maxPooling2d({ poolSize, strides }));
+
+    this.model.add(tf.layers.conv2d({
+      filters: filters * 3,
+      kernelSize,
+      activation: 'relu',
+      inputShape: [height, width, channels],
+    }));
+
+    this.model.add(tf.layers.maxPooling2d({ poolSize, strides }));
+
+    this.model.add(tf.layers.conv2d({
+      filters: filters * 4,
+      kernelSize,
+      activation: 'relu',
+      inputShape: [height, width, channels],
+    }));
+
+    this.model.add(tf.layers.maxPooling2d({ poolSize, strides }));
+
     this.model.add(tf.layers.flatten());
     this.model.add(tf.layers.dense({ units: fullyConnectedLayers, activation: 'relu' }));
     this.model.add(tf.layers.dense({ units: categories, activation: 'softmax' }));
 
-    // Define the training parameters
-    const optimizer = tf.train.adam(learningRate);
-
     this.model.compile({
-      optimizer: optimizer,
-      // optimizer: 'sgd',
+      optimizer: tf.train.adam(learningRate),
       loss: 'categoricalCrossentropy',
-      // loss: 'meanSquaredError'
       metrics: ['accuracy']
     });
   }
@@ -58,6 +82,9 @@ class CNN {
    * 
    */
   async imageToTensor (imagePath) {
+    const buffer = fs.readFileSync(imagePath);
+    return tf.node.decodeImage(buffer);
+/*
     return new Promise((resolve, reject) => {
         fs.createReadStream(imagePath)
           .pipe(new PNG())
@@ -82,6 +109,7 @@ class CNN {
         );
       }
     );
+*/
   }
 
   /**
@@ -124,15 +152,14 @@ class CNN {
     const images = await this.getTensor(imagePathOrTensorList)
     const imageTensors = tf.data.array(images);
     const labelTensors = tf.data.array(labels.map(label => tf.tensor1d(label, 'int32')));
-
     const dataset = tf.data.zip({ xs: imageTensors, ys: labelTensors })
-    //
 
     // Train the model
     await this.model.fitDataset(dataset.batch(this.batchSize), {
-      verbose,
+      verbose: true,
       epochs: this.epochs,
       batchSize: this.batchSize,
+      shuffle: true
     });
 
     return images
@@ -143,7 +170,6 @@ class CNN {
    */
   async predict (imagePathOrTensor) {
     const imageTensor = await this.getTensor(imagePathOrTensor)
-    // const preparedImageTensor = imageTensor.reshape(this.batchSize, this.height, this.width, this.channels)
     const predictions = this.model.predict(imageTensor.expandDims());
     return predictions
   }
--- tests/cnn.js
+++ tests/cnn.js
@@ -2,32 +2,35 @@ const CNN = require('../cnn');
 
 async function main() {
 	const trainingData = {
-		'images/2010.elonmusk.png': [0],
-		'images/2011.elonmusk.png': [1],
-		'images/2012.elonmusk.png': [2],
-		'images/2013.elonmusk.png': [3],
-		'images/2014.elonmusk.png': [4],
-		'images/2015.elonmusk.png': [5],
-		'images/2016.elonmusk.png': [6],
-		'images/2017.elonmusk.png': [7],
-		'images/2018.elonmusk.png': [8],
-		'images/2019.elonmusk.png': [9],
-		'images/2020.elonmusk.png': [10],
-		'images/2021.elonmusk.png': [11],
-		'images/2022.elonmusk.png': [12],
-		'images/2023.elonmusk.png': [13],
+		'images/2010.elonmusk.png': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		'images/2011.elonmusk.png': [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		'images/2012.elonmusk.png': [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		'images/2013.elonmusk.png': [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		'images/2014.elonmusk.png': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		'images/2015.elonmusk.png': [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+		'images/2016.elonmusk.png': [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+		'images/2017.elonmusk.png': [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+		'images/2018.elonmusk.png': [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+		'images/2019.elonmusk.png': [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+		'images/2020.elonmusk.png': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+		'images/2021.elonmusk.png': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+		'images/2022.elonmusk.png': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+		'images/2023.elonmusk.png': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 	}
 
+	const filepaths = Object.keys(trainingData)
 	const labels = Object.values(trainingData)
 
 	const cnn = new CNN({
-		categories: Math.max(...labels)
+		categories: trainingData[filepaths[0]].length
 	})
 
-	const images = await cnn.train(Object.keys(trainingData), labels)
-	const predictions = await cnn.predict(images[1])
-	const closest = predictions.argMax().dataSync()[0]
-	console.log(closest)
+	const images = await cnn.train(filepaths, labels)
+	const index = 5
+	const predictions = await cnn.predict(images[index])
+	const confidence = predictions.arraySync()[0]
+	const maxConfidence  =Math.max(...confidence) 
+	console.log(`We are ${(maxConfidence * 100).toFixed(2)}% confident that the image at image index ${index} is at confidence index ${confidence.indexOf(maxConfidence)}`)
 }
 
 main();
